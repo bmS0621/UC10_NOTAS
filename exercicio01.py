8db6e6a880bd39ec3bd5d2b0f45eb43cfde0f255
Bruno <brunomacielbms@gmail.com>
2023-11-01 15:03:07 -0300

    codigo notas

---
+++ exercicio01.py
@@ -0,0 +1,14 @@
+print("Exercicio de GitHub")
+
+nome = input("Digite Seu Nome: ")
+
+print(f"O nome digitado foi: {nome}")
+
+nota1 = int(input("Digite uma nota: "))
+nota2 = int(input("Digite uma segunda nota: "))
+nota3 = int(input("Digite uma terceira nota: "))
+nota4 = int(input("Digite uma quarta nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+print(f"O aluno{nome} obeteve a média de{media}")
